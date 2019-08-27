cf62b24f90bd2a1bca4444460fb4a5177373ab19
Muzi-P <phiwokuhlegondwe@gmail.com>
2019-08-27 11:27:07 +0800

    Added lis

--- todo-ninja/src/components/Navbar.vue
+++ todo-ninja/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav>
-        <v-toolbar flat app>
-            <v-app-bar-nav-icon class="grey--text" @click="drawer=!drawer"></v-app-bar-nav-icon>
+        <v-app-bar flat app>
+            <v-app-bar-nav-icon class="grey--text" @click="drawer =! drawer"></v-app-bar-nav-icon>            
             <v-toolbar-title class="text-uppercase grey--text">
                 <span class ="font-weight-light">Todo</span>
                 <span>Ninja</span>                
@@ -11,13 +11,19 @@
                 <span>Sign Out</span>
                 <v-icon right>exit_to_app</v-icon>
             </v-btn>
-        </v-toolbar>
-
-        <!-- Show navigation drawer -->
-
-        <v-navigation-drawer v-model="drawer" app class="indigo">
-            <p>Test</p>
+        </v-app-bar>
+       
+        <v-navigation-drawer  v-model="drawer" app temporary= "true" overlay  class="primary">
+           <v-list>             
+               <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                    <v-list-item-action>
+                        <v-icon class="white--text">{{link.icon}}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-title class="white--text">{{link.text}}</v-list-item-title>
+               </v-list-item>
+           </v-list>
         </v-navigation-drawer>
+
     </nav>
 </template>
 
@@ -26,7 +32,12 @@
 export default {
     data(){
         return {
-            drawer: false
+            drawer: false,
+            links: [
+                { icon: 'dashboard', text: 'Dashboard', route: '/' },
+                { icon: 'folder', text: 'My Projects', route: '/projects' },
+                { icon: 'person', text: 'Team', route: '/team' },
+            ]
         }
     }
 }
