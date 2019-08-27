22a077454810deb189192e4f5f9779e46676a01a
Muzi-P <phiwokuhlegondwe@gmail.com>
2019-08-27 14:38:20 +0800

    Added Project data

--- todo-ninja/src/views/Projects.vue
+++ todo-ninja/src/views/Projects.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="team">
-    <h1> Project</h1>   
+    <h1 class="subheading grey--text"> Project</h1>   
+     <v-container class="my-5">
+        content
+    </v-container>
     
   </div>
 </template>
